0622c09e67fa9819df1b26ba57df0e6dc79941c5
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-17 10:00:19 +0300

    Добавьте файлы проекта.

---
+++ Pen/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Pen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen() { 
+            color = "Черный";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int penCost) { 
+            color = penColor;
+            cost = penCost;
+        }
+    }
+}
---
+++ Rectangle/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class Rectangle
+    {
+        public int a;
+        public int b;
+
+        public int Square()
+        { 
+            return a * b; 
+        }
+
+        public Rectangle() {
+            a = 6;
+            b = 4;
+        }
+
+        public Rectangle(int a)
+        {
+            this.a = a;
+            this.b = a;
+        }
+
+        public Rectangle(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
