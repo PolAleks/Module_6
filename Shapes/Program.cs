06cad797fb35a76b257cfe32633fd7c1a21f9f08
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-22 12:46:37 +0300

    6.5.2 - Создание класа Shapes

---
+++ Shapes/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Triangle
+    {
+        public int sideA;
+        public int sideB;
+        public int sideC;
+
+        public int Perimetr()
+        {
+            return 0;
+        }
+
+        public int Area() 
+        { 
+            return 0;
+        }
+    }
+
+    public class Square
+    {
+        public int side;
+
+        public int Perimetr()
+        {
+            return 0;
+        }
+
+        public int Area()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle
+    {
+        public double radius;
+
+        public double Perimetr()
+        {
+            return 0;
+        }
+
+        public double Area()
+        {
+            return 0;
+        }
+    }
+}
