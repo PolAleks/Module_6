9a4ff8121b06068c9ec6bcee0418ffa20a75626a
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-22 12:03:26 +0300

    6.3.2 - Класс Bus  с методом PrintStatus вывод кол-во пассажиров

---
+++ Bus/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Bus
+{
+    internal class Program
+    {
+        class Bus
+        {
+            public int? Load;
+
+            public void PrintStatus()
+            {
+                if (Load.HasValue)
+                    Console.WriteLine($"В автобусе {Load.Value} пассжиров");
+                else
+                    Console.WriteLine($"Автобус пустой");
+            }
+            public Bus() 
+            {
+                Load = null;
+            }
+
+            public Bus(int? load)
+            {
+                Load = load;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Bus bus = new Bus();
+            bus.PrintStatus();
+
+            bus = new Bus(30);
+            bus.PrintStatus();
+        }
+    }
+}
---
+++ Department/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Department
+{
+    internal class Program
+    {
+        class Company
+        {
+            public string Type;
+            public string Name;
+
+            public Company()
+            {
+                Type = "Неизвестная компания";
+            }
+
+            public Company(string type)
+            {
+                this.Type = type;
+            }
+
+        }
+
+        class Department
+        {
+            public Company company;
+            public City city;
+
+            
+            public Department(Company company, City city)
+            {
+                company = company ?? new Company("Банк");
+                city = city ?? new City("Санкт-Петербург");
+            }
+        }
+
+        class City
+        {
+            public string Name;
+
+            public City()
+            {
+               
+            }
+            public City(string name)
+            {
+                Name = name;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var department = GetCurrentDepartment();
+            Console.WriteLine($"У банка {department.company.Type} есть отделение в Санкт-Петербурге");
+        }
+        static Department GetCurrentDepartment()
+        {
+            return new Department(new Company(), new City());
+        }
+    }
+}
