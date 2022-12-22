03b15fd918b40f3f607b5d4aaa243c7671b74089
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-22 16:44:37 +0300

    6.6.1 - Создание класса TrafficLight - светофор

---
+++ TrafficLight/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Dynamic;
+
+namespace TrafficLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var instace = new TrafficLight();
+            string color = instace.GetColor();
+            Console.WriteLine(color);
+        }
+    }
+    internal class TrafficLight
+    {
+        private Color color;
+
+        public TrafficLight()
+        {
+            this.color = Color.Red;
+        }
+
+        private void ChangeColor(Color color)
+        { 
+            this.color = color; 
+        }
+
+        public string GetColor ()
+        {
+            return this.color.ToString();
+        }
+    }
+
+    enum Color
+    {
+        Red = 0,
+        Yellow,
+        Green
+    }
+}
